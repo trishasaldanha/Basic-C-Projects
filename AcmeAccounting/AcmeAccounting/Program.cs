34faa92f97eb8afbdc241aa47236e11498d40940
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-03 11:23:29 -0700

    Added files for Acme Accounting Project

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
