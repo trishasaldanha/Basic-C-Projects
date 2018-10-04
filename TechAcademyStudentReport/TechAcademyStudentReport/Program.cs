fb261c4bb72e4f84a28d4ae31cde233e56021969
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-04 09:48:21 -0700

    Added files for Tech Academy Student Report Drill

---
+++ TechAcademyStudentReport/TechAcademyStudentReport/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace TechAcademyStudentReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number are you on?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? (Please answer \"true\" or \"false\")");
+            bool helpNeeded = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string posExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = int.Parse(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
