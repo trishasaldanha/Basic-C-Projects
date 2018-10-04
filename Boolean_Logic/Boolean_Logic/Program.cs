c89b4ab6f67f9379cd7de0925201cb5c728bcb6e
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-04 13:58:03 -0700

    Added files for Tech Academy Boolean Logic Drill

---
+++ Boolean_Logic/Boolean_Logic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && speedingTickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
