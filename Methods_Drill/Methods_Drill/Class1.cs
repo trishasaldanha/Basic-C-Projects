d50e5f04da934e61efe9a3fae3da6b7519fe523b
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-09 18:23:44 -0700

    Added files for Tech Academy Methods Drill

---
+++ Methods_Drill/Methods_Drill/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill
+{
+    class Class1
+    {
+        static public int addMe(int numOne)
+        {
+            int tempValue = numOne + 100;
+            return tempValue;
+        }
+
+        static public int multiplyMe(int numOne)
+        {
+            int tempValue = numOne * 25;
+            return tempValue;
+        }
+
+        static public int subtractMe(int numOne)
+        {
+            int tempValue = numOne - 5;
+            return tempValue;
+        }
+    }
+}
---
+++ Methods_Drill/Methods_Drill/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digitOne = 0, finalAnswer1, finalAnswer2, finalAnswer3;
+            Console.WriteLine("Pick a number.");
+            digitOne = Convert.ToInt32(Console.ReadLine());
+
+            finalAnswer1 = Class1.addMe(digitOne);
+            finalAnswer2 = Class1.multiplyMe(digitOne);
+            finalAnswer3 = Class1.subtractMe(digitOne);
+            Console.WriteLine(digitOne + " plus 100 is " + finalAnswer1 + ".");
+            Console.WriteLine(digitOne + " multiplied by 25 is " + finalAnswer2 + ".");
+            Console.WriteLine(digitOne + " minus 5 is " + finalAnswer3 + ".");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
