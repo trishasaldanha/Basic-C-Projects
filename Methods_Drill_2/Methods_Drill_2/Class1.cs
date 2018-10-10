b1a6d4c9b164f5f3aea93ed721514f562aae8f2d
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-10 10:58:18 -0700

    Added files for second Tech Academy Methods drill

---
+++ Methods_Drill_2/Methods_Drill_2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill_2
+{
+    class Class1
+    {
+        static public int MultiplyMe(int num1)
+        {
+            int tempValue = num1 * 72;
+            return tempValue;
+        }
+
+        static public decimal MultiplyMe(decimal num1)
+        {
+            int tempValue = Convert.ToInt32(num1 * 1000);
+            return tempValue;
+        } 
+
+        static public string MultiplyMe(string num1)
+        {
+            int tempValue = Convert.ToInt32(num1) * 17;
+            return Convert.ToString(tempValue);
+        }
+    }
+}
---
+++ Methods_Drill_2/Methods_Drill_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(Class1.MultiplyMe(12));
+            Console.ReadLine();
+
+            Class1 class2 = new Class1();
+            int finalAnswer2 = Convert.ToInt32(Class1.MultiplyMe(7.8m));
+            Console.WriteLine(finalAnswer2);
+            Console.ReadLine();
+
+            Class1 class3 = new Class1();
+            int finalAnswer3 = Convert.ToInt32(Class1.MultiplyMe("8"));
+            Console.WriteLine(finalAnswer3);
+            Console.ReadLine();
+        }
+    }
+}
