c0c899b40f05edc6f60caf13a40c8e7e60fa4ee2
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-10 10:39:48 -0700

    Added files for Tech Academy Void Method Drill

---
+++ Void_Method_Drill/Void_Method_Drill/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_Drill
+{
+    class Class1
+    {
+        public static void DoSomething(int num1, int num2)
+        {
+            int operationOne = num1 * 44;
+            Console.WriteLine(num2);
+            //Console.WriteLine(operationOne);
+            return;
+        }
+    }
+}
---
+++ Void_Method_Drill/Void_Method_Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Class1.DoSomething(17, 25);
+            Class1.DoSomething(num1: 300, num2: 75);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
