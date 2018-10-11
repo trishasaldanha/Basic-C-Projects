184577d32a85dbb23cbdc0ee8a5b2fba427305e9
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-11 06:49:43 -0700

    Added files for Tech Academy Drill

---
+++ Employee_Methods_Drill/Employee_Methods_Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Methods_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Student";
+            employee.LastName = "Sample";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
