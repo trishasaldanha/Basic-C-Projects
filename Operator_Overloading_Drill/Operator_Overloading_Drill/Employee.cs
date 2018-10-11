8ab656451f597160847b016719e9f765f2cfbf8c
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-11 15:38:33 -0700

    Added files for Tech Academy Operator Overloading drill

---
+++ Operator_Overloading_Drill/Operator_Overloading_Drill/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Drill
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.Id == employee2.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.Id != employee2.Id)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ Operator_Overloading_Drill/Operator_Overloading_Drill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            employee1.Id = 678;
+            employee2.Id = 452;
+
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
