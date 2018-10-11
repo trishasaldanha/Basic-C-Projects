fefe213d8fb7cd567b81ed3da9049ce30cc77c98
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-11 10:56:34 -0700

    Added files for Tech Academy Interface Drill

---
+++ Interface_Drill/Interface_Drill/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Drill
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This person is no longer employed here.");
+        }
+    }
+}
---
+++ Interface_Drill/Interface_Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee = new Employee();
+            
+            employee.Quit();
+            Console.ReadLine();
+            
+        }
+    }
+}
