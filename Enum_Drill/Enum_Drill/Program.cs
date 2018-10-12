77d1b25647f092a0b3e96b977c588446b6d8d62d
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-12 11:28:13 -0700

    Added files for Tech Academy Enum Drill

---
+++ Enum_Drill/Enum_Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Drill
+{
+    class Program
+    {
+        enum DaysOfTheWeek { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a day of the week:");
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine(), true);
+                int underlyingValue = Convert.ToInt32(day);
+                Console.WriteLine("That is right, that is a day of the week!", underlyingValue);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+            
+        }
+
+    }
+}
