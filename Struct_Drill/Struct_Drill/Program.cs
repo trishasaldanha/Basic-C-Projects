4230e7ead9ba8f9665e9fbcf545acb48073d1960
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-15 09:33:54 -0700

    Added files for Tech Academy Struct drill.

---
+++ Struct_Drill/Struct_Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 76.8m;
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+            
+        }
+    }
+}
