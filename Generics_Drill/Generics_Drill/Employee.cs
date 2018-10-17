b57545db99cfcf8a99ed8ce1b24460d9c968e4a8
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-17 11:18:01 -0700

    Added files for Tech Academy Generics Drill

---
+++ Generics_Drill/Generics_Drill/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Drill
+{
+    public class Employee<T> 
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Generics_Drill/Generics_Drill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] string1 = { "one", "two" };
+            Employee<string> emp1 = new Employee<string>();
+            {
+                emp1.Id = 7;
+                emp1.Things = string1.ToList<string>();
+            };
+            int[] int1 = { 1, 2 };
+            Employee<int> intList = new Employee<int>();
+            {
+                intList.Id = 9;
+                intList.Things = int1.ToList<int>();
+            }
+            for (int i = 0; i < emp1.Things.Count; i++)
+            {
+                Console.WriteLine(emp1.Things[i]);
+            }
+            for (int j = 0; j < intList.Things.Count; j++)
+            {
+                Console.WriteLine(intList.Things[j]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
