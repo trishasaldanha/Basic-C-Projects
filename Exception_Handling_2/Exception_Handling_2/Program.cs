1bbbb48f0cea2fa7f4346e25819edfad8bec1760
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-17 11:58:04 -0700

    Added files for Tech Academy Exception Handling drill

---
+++ Exception_Handling_2/Exception_Handling_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge < 1)
+                {
+                    Console.WriteLine("Please enter a valid age. (No negative numbers or zero.)");
+                    Console.ReadLine();
+                    return;
+                }
+                int UserAgeDays = userAge * 365;
+                TimeSpan yearsAlive = TimeSpan.FromDays(UserAgeDays);
+                DateTime now = DateTime.Now;
+                DateTime dateOfBirth = now - yearsAlive;
+                Console.WriteLine(dateOfBirth.Year);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
