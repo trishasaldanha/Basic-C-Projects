21c476d00295dcc04ddf62ae632eafa0a820fb70
Trisha Saldanha <trisha.saldanha88@gmail.com>
2018-10-17 09:54:35 -0700

    Added files for Tech Academy Methods Drill 3

---
+++ Methods_Drill_3/Methods_Drill_3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill_3
+{
+    class Class1
+    {
+        public static int SomeMethod(int num1, int num2 =5)
+        {
+            int tempValue = num1 * num2;
+            return tempValue;
+        }
+    }
+}
---
+++ Methods_Drill_3/Methods_Drill_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Drill_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an additional number or just press enter if you would like to skip this number.");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Class1.SomeMethod(num1, num2));
+                Console.ReadLine();
+                return;
+            }
+            
+            catch
+            {
+                Console.WriteLine(Class1.SomeMethod(num1));
+                Console.ReadLine();
+                return;
+            }
+            
+        }
+    }
+}
